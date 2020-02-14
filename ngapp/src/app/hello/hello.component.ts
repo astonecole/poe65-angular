3f5dbb163998492703f5b3bc48c1f9a3a62468c6
Vincent Letourneau <vincent@nanoninja.com>
2020-02-14 09:13:53 +0100

    Add Hello Wolrd Component

---
+++ ngapp/src/app/hello/hello.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/**
+ * selector: app-hello | [app-hello] | .app-hello
+ */
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class HelloComponent implements OnInit {
+  name: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
