8aa1f5f9f0d1f850a4decc52818ca474dad854bd
Vincent Letourneau <vincent@nanoninja.com>
2020-02-19 13:47:07 +0100

    Fix notification

--- ngapp/src/app/flash-messenger/flash-messenger.component.ts
+++ ngapp/src/app/flash-messenger/flash-messenger.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from './message.model';
 import { FlashMessengerService } from '../flash-messenger.service';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flash-messenger',
@@ -17,14 +16,13 @@ export class FlashMessengerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.messageService.notify('Message depuis notify');
-
     this.messageService.messages$
-      .pipe(tap(value => console.log(value)))
-      .subscribe(
-        (message: Message) => this.messages.push(message),
-        err => console.log(err)
+    .subscribe(
+      (message: Message) => this.messages.push(message),
+      err => console.log(err)
       );
+  
+      this.messageService.notify('Message depuis notify', 'success');
   }
 
   onClose(pos: number): void {
